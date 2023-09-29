e0dd5bca500645c35d8c7e8f5a52c95102423935
papus <asoroko@interfarmax.by>
2023-09-29 15:37:54 +0300

    Добавьте файлы проекта.

---
+++ MyTestApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetName")]
+        public string Get(string name = "empty")
+        {
+            return $"Your name is {name}";
+        }
+    }
+}
